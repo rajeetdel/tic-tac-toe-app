6e1ab3419e84bc6e47526c360f037e5fdf801169
Rajeet <rajeetdel@gmail.com>
2019-07-09 17:38:36 +0530

    Updates

---
+++ precache-manifest.3852840033798bedbfda74b49e62fdcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393d663e32fc2dd0b1d11b113331d9c2",
+    "url": "/tic-tac-toe-app/index.html"
+  },
+  {
+    "revision": "5dc66e424d1a23f007fa",
+    "url": "/tic-tac-toe-app/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe-app/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "5dc66e424d1a23f007fa",
+    "url": "/tic-tac-toe-app/static/js/main.28835c9d.chunk.js"
+  },
+  {
+    "revision": "bc4c146376c29eab1c80",
+    "url": "/tic-tac-toe-app/static/js/runtime~main.43fd7a8c.js"
+  }
+]);
